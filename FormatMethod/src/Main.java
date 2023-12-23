e2ec4dfe8afb9821cbe2729444537b096801c2e8
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2023-12-23 19:58:26 +0330

    tamrin hands on jalase 2

---
+++ FormatMethod/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String fullName = "AmirHossein Jahankheir Amlashi";
+        Byte age = 25;
+        String data = String.format("Full Name: %s, Age: %d", fullName, age);
+        System.out.println(data);
+    }
+}
